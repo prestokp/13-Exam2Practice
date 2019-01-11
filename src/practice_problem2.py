661f381ae7136da31cd42dcb1c2d3b805a831a74
prestokp <prestokp@rose-hulman.edu>
2019-01-11 00:07:38 -0500

    Paper and Pencil helps

--- src/practice_problem2.py
+++ src/practice_problem2.py
@@ -194,7 +194,7 @@ def practice_problem2b(sequence):
       :type sequence: [str]
     """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
